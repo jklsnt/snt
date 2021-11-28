3d90f0e773a3db49256c2c4919fb914835ed03c3
zbuster05 <zakary.saya@gmail.com>
2021-11-28 01:11:06 -0800

    Slightly more reasonable

--- squares/src/components/TaskSquareDay.jsx
+++ squares/src/components/TaskSquareDay.jsx
@@ -5,9 +5,9 @@ export default function TaskSquareDay(props) {
     return <div className="flex w-5/6 py-0 px-0 m-auto bg-gray-800 rounded-2xl flex-wrap items-baseline" style={{marginTop: -20+"px", maxWidth: 507+'px'}}>
 	       
 	       <TaskSquare textstuff="Brush Teeth" textstuff2={2} textstuff3={3} colors={[37, 99, 235, 1]}></TaskSquare>
-	       <TaskSquare textstuff="Eat Dog" textstuff2={0} textstuff3={1} colors={[220, 72, 5, 1]}></TaskSquare>
-	       <TaskSquare textstuff="Burry Bodies" textstuff2={1} textstuff3={1} colors={[20, 20, 20, 1]}></TaskSquare>	       <TaskSquare textstuff="Brush Teeth" textstuff2={2} textstuff3={3} colors={[37, 99, 235, 1]}></TaskSquare>
-	       <TaskSquare textstuff="Eat Dog" textstuff2={0} textstuff3={1} colors={[220, 72, 5, 1]}></TaskSquare>
+	       <TaskSquare textstuff="Eat Dog" textstuff2={0} textstuff3={1} colors={[210, 72, 5, 1]}></TaskSquare>
+	       <TaskSquare textstuff="Burry Bodies" textstuff2={1} textstuff3={1} colors={[140, 130, 13, 1]}></TaskSquare>	       <TaskSquare textstuff="Brush Teeth" textstuff2={2} textstuff3={3} colors={[37, 99, 235, 1]}></TaskSquare>
+	       <TaskSquare textstuff="Eat Dog" textstuff2={0} textstuff3={1} colors={[210, 72, 5, 1]}></TaskSquare>
 
 	   </div>
 }
