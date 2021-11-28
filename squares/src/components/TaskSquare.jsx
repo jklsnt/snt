235cd2a8177118967ab214061e85cfdac6833efa
zbuster05 <zakary.saya@gmail.com>
2021-11-28 00:31:46 -0800

    Thingys workish

--- squares/src/components/TaskSquare.jsx
+++ squares/src/components/TaskSquare.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 export default function TaskSquare(props) {
-    return <div className="m-1 rounded-lg bg-blue-600" style={{width: 7+"rem", height: 7+"rem"}}></div>
+    return <div className="m-5 rounded-lg bg-blue-600 text-center text-gray-300" style={{minWidth: 8+"rem", minHeight: 7.3+"rem"}}>
+	       <div className="mt-1">{props.textstuff}</div>
+	       <div className="font-mono mt-2 text-4xl">{props.textstuff2+'/'+props.textstuff3}</div>
+	   </div>
 }
 
