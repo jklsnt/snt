45cc8801bbdf79bfa280317e6a26901dc77333cc
zbuster05 <zakary.saya@gmail.com>
2021-11-28 00:04:11 -0800

    Spacing looks a lot better

--- squares/src/components/TaskSquareDay.jsx
+++ squares/src/components/TaskSquareDay.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import TaskSquare from './TaskSquare'
 
 export default function TaskSquareDay(props) {
-    return <div className="flex w-5/6 py-12 m-auto my-16 bg-gray-800 rounded-2xl">
+    return <div className="flex w-5/6 py-5 m-auto bg-gray-800 rounded-2xl" style={{marginTop: -20+"px"}}>
 	       <TaskSquare textstuff="urbad" textstuff2={2}></TaskSquare>
 	   </div>
 }
