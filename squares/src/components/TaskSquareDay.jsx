3886064f221875a702dcf2c0637c1fb8e2dfbcfe
zbuster05 <zakary.saya@gmail.com>
2021-11-27 21:56:36 -0800

    THing 2

---
+++ squares/src/components/TaskSquareDay.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import TaskSquare form './TaskSquare.jsx'
+
+export default function TaskSquareDay(props) {
+    return <div className="flex">
+	       <TaskSquare></TaskSquare>
+	   </div>
+}
