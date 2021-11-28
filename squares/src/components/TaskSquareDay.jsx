22a6b6ca4a40bc9e6519f4127d11a8abbb302663
zbuster05 <zakary.saya@gmail.com>
2021-11-28 00:39:26 -0800

    Finneet e

--- squares/src/components/TaskSquareDay.jsx
+++ squares/src/components/TaskSquareDay.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import TaskSquare from './TaskSquare'
 
 export default function TaskSquareDay(props) {
-    return <div className="flex w-5/6 py-0 px-0 m-auto bg-gray-800 rounded-2xl flex-wrap items-center" style={{marginTop: -20+"px"}}>
+    return <div className="flex w-5/6 py-0 px-0 m-auto bg-gray-800 rounded-2xl flex-wrap items-baseline" style={{marginTop: -20+"px", maxWidth: 507+'px'}}>
 	       
 	       <TaskSquare textstuff="Brush Teeth" textstuff2={2} textstuff3={3}></TaskSquare>
 	       <TaskSquare textstuff="Brush Teeth" textstuff2={2} textstuff3={3}></TaskSquare>
