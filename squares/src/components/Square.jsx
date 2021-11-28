4dea61d12cde8a1d63ab552570ccbf9d6eb46271
Exr0n <mail@exr0n.com>
2021-11-27 23:01:43 -0800

    rbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb

--- squares/src/components/Square.jsx
+++ squares/src/components/Square.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
 export default function Square(props) {
-    return <div className="w-12 h-12 m-1 rounded-md" style={{"backgroundColor": `rgba(50, 108, 204, ${props.data / props.scalar})`}}></div>
+    return <div className="w-12 h-12 m-1 rounded-md" style={{"backgroundColor": `rgba(29, 88, 216, ${props.data / props.scalar})`}}></div>
 }
 
