2168e6b1d41698f76fbe9332709e9f1a14b8c890
zbuster05 <zakary.saya@gmail.com>
2021-11-28 00:47:56 -0800

    Setting colors works

--- squares/src/components/TaskSquare.jsx
+++ squares/src/components/TaskSquare.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export default function TaskSquare(props) {
-    return <div className="m-5 rounded-lg bg-blue-600 text-center text-gray-300" style={{minWidth: 8+"rem", minHeight: 7.3+"rem"}}>
+    return <div className="m-5 rounded-lg text-center text-gray-300" style={{minWidth: 8+"rem",
+											 minHeight: 7.3+"rem",
+											 backgroundColor: "rgba(" + props.colors[0] + ","+props.colors[1]+"," + props.colors[2] + ")"}}>
 	       <div className="mt-1">{props.textstuff}</div>
 	       <div className="font-mono mt-2 text-4xl">{props.textstuff2+'/'+props.textstuff3}</div>
 	   </div>
