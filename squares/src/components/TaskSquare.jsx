97c80ab953cf454a3ee07e7bba30eca3efd416d2
Exr0n <mail@exr0n.com>
2021-11-29 08:17:28 -0800

    pre state

--- squares/src/components/TaskSquare.jsx
+++ squares/src/components/TaskSquare.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 export default function TaskSquare(props) {
-    return <div className="m-5 rounded-lg text-center text-gray-300 font-bold" style={{minWidth: 8+"rem",
+    console.log(props.textstuff2, props.textstuff3, 0.1 + props.textstuff2 / props.textstuff3 * 0.9)
+    return <button onClick={props.onClick} className="m-5 rounded-lg text-center text-gray-300 font-bold" style={{minWidth: 8+"rem",
 											 minHeight: 7.3+"rem",
-											 backgroundColor: "rgba(" + props.colors[0] + ","+props.colors[1]+"," + props.colors[2] + props.colors[3] + ")"}}>
+											 backgroundColor: "rgba(" + props.colors[0] + ","+props.colors[1]+"," + props.colors[2] + ',' + (0.1 + props.textstuff2 / props.textstuff3 * 0.9) + ")"}}>
 	       <div className="mt-1 font-extrabold">{props.textstuff}</div>
 	       <div className="font-mono mt-2 text-4xl">{props.textstuff2+'/'+props.textstuff3}</div>
-	   </div>
+	   </button>
 }
 
--- squares/src/components/TaskSquareDay.jsx
+++ squares/src/components/TaskSquareDay.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import TaskSquare from './TaskSquare'
 
 export default function TaskSquareDay(props) {
+    const buttons = [
+        {'name': "Brush Teeth", "current": 1, "needed": 2, 'colors': [37, 99, 235]},
+        {'name': "Burrie Body", "current": 0, "needed": 1, 'colors': [218, 72, 5]},
+        {'name': "Meditate", "current": 1, "needed": 1, 'colors': [134, 35, 171]},
+    ]
+    console.log('hrrrrrrrrrrrrrrrrrrrrrrrrr')
     return <div className="flex w-5/6 py-0 px-0 m-auto bg-gray-800 rounded-2xl flex-wrap items-baseline" style={{marginTop: -20+"px", maxWidth: 507+'px'}}>
-	       
-	       <TaskSquare textstuff="Brush Teeth" textstuff2={1} textstuff3={2} colors={[37, 99, 235, 1]}></TaskSquare>
-	       <TaskSquare textstuff="Burrie Body" textstuff2={0} textstuff3={1} colors={[210, 72, 5, 1]}></TaskSquare>
-	       <TaskSquare textstuff="Meditate" textstuff2={1} textstuff3={1} colors={[134, 35, 171, 1]}></TaskSquare>	   
+        {
+            buttons.map((attrs, i) => {
+                function clickHandler(e) {
+                    console.log(e.target.props.textstuff2)
+                    attrs.current = (attrs.current+1) % attrs.needed;
+                }
+                return <TaskSquare textstuff={attrs['name']} textstuff2={attrs['current']} textstuff3={attrs['needed']} colors={attrs['colors']} onClick={clickHandler} key={i} />
+            })
+        }
 	   </div>
 }
     
