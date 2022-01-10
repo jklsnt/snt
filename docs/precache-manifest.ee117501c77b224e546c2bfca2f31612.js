727cd476c004f7a9d29180eabe2d57aeb6f09836
Exr0n <mail@exr0n.com>
2022-01-09 19:14:28 -0800

    serve time

---
+++ docs/precache-manifest.ee117501c77b224e546c2bfca2f31612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7654cca959da535a1ae3d63703470c5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0fa08b63835b2b431e8",
+    "url": "/static/css/main.d78ec76a.chunk.css"
+  },
+  {
+    "revision": "49bea9b5719d033eda34",
+    "url": "/static/js/2.71cf95f9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.71cf95f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0fa08b63835b2b431e8",
+    "url": "/static/js/main.cdd44c3f.chunk.js"
+  },
+  {
+    "revision": "6613802de99d406509fd",
+    "url": "/static/js/runtime-main.8d5285fa.js"
+  }
+]);
