7c683563191089c9df037f0fe557332591cb2ed5
Exr0n <mail@exr0n.com>
2022-01-15 16:53:42 -0800

    that took wayyyyyyyy to long

--- squares/src/App.js
+++ squares/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 //import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import googleOauthImage from './google_signin_buttons/web/2x/btn_google_signin_dark_normal_web@2x.png';
@@ -14,17 +14,37 @@ function Login() {
         if (error) console.error(error);    // TODO: we should probably handle auth errors
     }
     return <div className="relative flex w-screen h-screen overflow-hidden bg-gray-800 space-around">
-            <div className="block w-1/3 p-10 m-auto text-center text-white border border-red-200 bg-gradient-to-tr from-blue-700 to-purple-600 rounded-xl max-w-prose space-y-10"><span>mogus tracker</span>
+            <div className="block w-1/3 p-10 m-auto text-center text-white bg-gradient-to-tr from-blue-700 to-purple-600 rounded-xl max-w-prose space-y-10"><span>mogus tracker</span>
 
-                <div className="flex-col content-center justify-center border-2 border-red-300 align-center">
-                    <img className="inline-block h-12 rounded-lg" onClick={signInWithGoogle} src={googleOauthImage} />
+                <div className="flex-col content-center justify-center align-center">
+                    <button onClick={signInWithGoogle}><img className="inline-block h-12 rounded-lg" src={googleOauthImage} /></button>
                 </div>
             </div>
         </div>;
 }
 
+function logOut(setter) {
+    console.log('user pre log out', supabase.auth.user());
+    supabase.auth.signOut()
+        .then(() => { setter(false) });
+}
+function logIn(provider, setter) {
+    supabase.auth.signIn({ provider: provider })
+        .then(() => { setter(true) });
+        //.then(({ error }) => { if (error) console.error(error); });
+    setter(true);
+}
+
 export default function() {
-    if (supabase.auth.user() !== null)
-        return <Dashboard />;
-    else return <Login />
+    const [ authed, setAuthed ] = useState(supabase.auth.user() !== null);
+
+    supabase.auth.onAuthStateChange((_, session) => {
+        if (session?.user) setAuthed(true);
+        else setAuthed(false);
+    });
+
+    return <div>
+        { !authed ? <Login logIn={(provider) => { logIn(provider, setAuthed) }} /> :
+            <Dashboard logOut={() => { logOut(setAuthed) }} /> }
+    </div>
 }
--- squares/src/Dashboard.js
+++ squares/src/Dashboard.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useReducer } from 'react';
+import Confetti from 'react-confetti';
+import LogoutIcon from '@mui/icons-material/Logout';
+
 import SquaresDisplay from './components/SquaresDisplay';
 import TaskSquareDay from './components/TaskSquareDay';
 
+//import supabase from './supabase';
 import squareReducer from './squareReducer';
 import { completion } from './semantics';
 import { mapUntil, zip } from './util';
@@ -12,7 +16,9 @@ import { mapUntil, zip } from './util';
 //    return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() + 1) / 7);
 //}
 
-function Dashboard() {
+function Dashboard({ logOut }) {
+    //supabase.auth.signOut().then(console.log);
+    //console.log("signed out")
     const og_thing = [[1, 1, 0], [4, 4, 6], [2, 5, 6], [3, 1, 3], [4, 2, 0], [1, 0, 1], [3, 4, 3], [2, 4, 4], [2, 3, 2], [1, 2, 2], [4, 1, 0], [0, 0, 2], [1, 0, 3], [3, 1, 1], [3, 4, 0], [1, 0, 0], [4, 1, 1], [0, 0, 0], [1, 0, 3], [4, 1, 4], [2, 1, 3], [1, 2, 3], [4, 4, 2], [2, 1, 2], [1, 0, 1], [3, 0, 4], [2, 3, 4], [1, 4, 4], [3, 0, 4], [2, 3, 4], [0, 2, 2], [4, 0, 4], [3, 4, 4], [2, 1, 0], [0, 0, 0]];
     const maxima = og_thing.map(_ => [2, 1, 3]);
 
@@ -22,16 +28,28 @@ function Dashboard() {
             .map(([countd, maxima]) => completion(countd, maxima)),
         opacity => opacity >= 1)
         .reduce((a, c) => a + c, 0);
-    const streak_text = `${ completions }${ completion(squares.counts[0], squares.maxima[0]) >= 1? '+' : '' }`
+    const is_complete = completion(squares.counts[0], squares.maxima[0]) >= 1;
+    const streak_text = `${ completions }${ is_complete ? '+' : '' }`
 
     useEffect(() => { document.title = `${streak_text} days | mogus tracker` }, [streak_text]); // https://dev.to/luispa/how-to-add-a-dynamic-title-on-your-react-app-1l7k
 
+    return <div className="w-screen h-screen overflow-hidden bg-gray-800 ">
+        <Confetti numberOfPieces={1000} tweenDuration={15*1000} recycle={false}/>
+        <div className="flex items-center justify-center min-h-full ">
+            <div className="flex-col space-y-2">
+                <div className="text-xl font-bold text-center text-white ">you made it!</div>
+                <button className="flex px-6 py-3 text-white bg-gradient-to-tr from-blue-800 to-purple-600 space-x-2 rounded-xl" onClick={logOut}>
+                    <LogoutIcon fontSize="medium"/>
+                    <span>log out</span>
+                </button>
+            </div>
+        </div>
+    </div>;
+
     return (
-        <div className="fixed relative flex flex-col w-screen h-screen overflow-x-hidden bg-gray-900 select-none">
+    <div className="fixed relative flex flex-col w-screen h-screen overflow-x-hidden bg-gray-900 select-none">
+        { is_complete ? <Confetti numberOfPieces={1000} tweenDuration={15*1000} recycle={false}/> : <></> }
         <div className="mt-2 text-5xl font-extrabold text-center text-gray-500">MOGUS TRACKER</div>
-        {/*<div className="items-center justify-center font-mono text-4xl border-2 max-w-prose">
-                Hew
-            </div>*/}
         <div className="font-mono text-2xl text-gray-300 h-96 align-center">
             <div className="w-2/3 py-12 m-auto mt-5 bg-gray-800" style={{borderRadius: 70 + 'px', maxWidth: 349 + 'px', marginBottom: -20+"px", marginTop: -20+"px"}}>
                 <p className="text-center">Current Streak</p>
@@ -46,6 +64,9 @@ function Dashboard() {
         <div className="w-5/6 pb-12 m-auto mt-8 bg-gray-800 rounded-2xl" style={{maxWidth: 507+'px'}}>
             <SquaresDisplay squares_data={squares}></SquaresDisplay>
         </div>
+        </div>
+        <div className="fixed bottom-0 content-center justify-center inline-block w-full h-12 text-center text-white border-2 border-red-300 align-center">
+        epic
         </div>
     </div>
     );
