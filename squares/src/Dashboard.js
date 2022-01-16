92961e0c10e0e5927067d669093d3b65a980e51c
Exr0n <mail@exr0n.com>
2022-01-15 17:49:09 -0800

    integrate login to dashboard; why no ghpages work

--- squares/src/Dashboard.js
+++ squares/src/Dashboard.js
@@ -33,22 +33,9 @@ function Dashboard({ logOut }) {
 
     useEffect(() => { document.title = `${streak_text} days | mogus tracker` }, [streak_text]); // https://dev.to/luispa/how-to-add-a-dynamic-title-on-your-react-app-1l7k
 
-    return <div className="w-screen h-screen overflow-hidden bg-gray-800 ">
-        <Confetti numberOfPieces={1000} tweenDuration={15*1000} recycle={false}/>
-        <div className="flex items-center justify-center min-h-full ">
-            <div className="flex-col space-y-2">
-                <div className="text-xl font-bold text-center text-white ">you made it!</div>
-                <button className="flex px-6 py-3 text-white bg-gradient-to-tr from-blue-800 to-purple-600 space-x-2 rounded-xl" onClick={logOut}>
-                    <LogoutIcon fontSize="medium"/>
-                    <span>log out</span>
-                </button>
-            </div>
-        </div>
-    </div>;
-
     return (
     <div className="fixed relative flex flex-col w-screen h-screen overflow-x-hidden bg-gray-900 select-none">
-        { is_complete ? <Confetti numberOfPieces={1000} tweenDuration={15*1000} recycle={false}/> : <></> }
+        { is_complete ? <div className="fixed"><Confetti numberOfPieces={400} /></div> : <></> }
         <div className="mt-2 text-5xl font-extrabold text-center text-gray-500">MOGUS TRACKER</div>
         <div className="font-mono text-2xl text-gray-300 h-96 align-center">
             <div className="w-2/3 py-12 m-auto mt-5 bg-gray-800" style={{borderRadius: 70 + 'px', maxWidth: 349 + 'px', marginBottom: -20+"px", marginTop: -20+"px"}}>
@@ -61,12 +48,13 @@ function Dashboard({ logOut }) {
         <div className="w-2 text-xl"><br/></div>
         <TaskSquareDay counts1={squares.counts[0]} maxima={squares.maxima[0]} dispatch={sq_dispatch}/>
         <div className="w-full">
-        <div className="w-5/6 pb-12 m-auto mt-8 bg-gray-800 rounded-2xl" style={{maxWidth: 507+'px'}}>
-            <SquaresDisplay squares_data={squares}></SquaresDisplay>
+            <div className="w-5/6 m-auto mt-8 font-mono text-center text-gray-600 bg-gray-800 rounded-2xl" style={{maxWidth: 507+'px'}}>
+                <SquaresDisplay squares_data={squares}></SquaresDisplay>
+                the beginning of time
+            </div>
         </div>
-        </div>
-        <div className="fixed bottom-0 content-center justify-center inline-block w-full h-12 text-center text-white border-2 border-red-300 align-center">
-        epic
+        <div className="fixed bottom-0 flex flex-row-reverse w-full h-20 p-10">
+            <button onClick={logOut} className="text-white bg-gray-800 rounded-lg" style={{ padding: "10px" }}>logout <LogoutIcon /></button>
         </div>
     </div>
     );
