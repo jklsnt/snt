90f08e376ccdfdd8427d5e4a5387d4437e642467
Exr0n <mail@exr0n.com>
2022-01-15 16:54:29 -0800

    push login/out app to prod

---
+++ docs/build/precache-manifest.629c3f6c6e8cbd298c48fea9f6410065.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02713ebffbdb53764eb04f55a4c779b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc4d787521c2c6148739",
+    "url": "/static/css/main.fcff73d3.chunk.css"
+  },
+  {
+    "revision": "53680ad02a7649bef5e1",
+    "url": "/static/js/2.c35d18d8.chunk.js"
+  },
+  {
+    "revision": "1546ccc726f110f64aa20141d055c611",
+    "url": "/static/js/2.c35d18d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc4d787521c2c6148739",
+    "url": "/static/js/main.fe0e8402.chunk.js"
+  },
+  {
+    "revision": "6613802de99d406509fd",
+    "url": "/static/js/runtime-main.8d5285fa.js"
+  }
+]);
